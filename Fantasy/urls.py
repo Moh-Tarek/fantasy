bdd7cb3df8d413c452d6d110982dccb213173d9f
Moh-Tarek <moh.tarek93@gmail.com>
2020-03-14 00:54:51 +0200

    all_player_and_fixture

--- Fantasy/urls.py
+++ Fantasy/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='Fantasy-home'),
     path('about/', views.about, name='Fantasy-about'),
+    path('fixtures/', views.fixtures, name='Fantasy-fixtures'),
     path('allplayers/', views.allPlayers, name='Fantasy-allplayers'),
     path('squadselection/', views.squadSelectionView, name='Fantasy-squadSelection'),
     path('register/',views.register, name='Fantasy-register'),
