78fa5b9361ea34a97bfa5f6991093aaa319696a2
Moh-Tarek <moh.tarek93@gmail.com>
2020-03-14 21:50:13 +0200

    profile_pic_edit

--- Fantasy/views.py
+++ Fantasy/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import SquadSelection,FantasyRegister
 from django.views.generic import ListView,DetailView
+from django.views import View
 from datetime import datetime
 from django.db.models import Sum
 import os
