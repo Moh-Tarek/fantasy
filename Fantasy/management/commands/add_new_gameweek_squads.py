3781c7d47f86bfc97085e59380ac583ca3d7694d
Yasser Mohsen <51789109+mohseny@users.noreply.github.com>
2020-03-14 23:56:00 +0200

    cron job to add new gameweek squads

--- Fantasy/management/commands/add_new_gameweek_squads.py
+++ Fantasy/management/commands/add_new_gameweek_squads.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from Fantasy.models import FantasySquad
+from Fantasy.models import FantasySquad, Player
 import os
 
 class Command(BaseCommand):
@@ -13,18 +13,19 @@ class Command(BaseCommand):
         return False
 
     def handle(self, *args, **options):
-        c = 0
-        current_gameweek = int(os.getenv('GAMEWEEK'))
-        previous_gameweek = current_gameweek - 1
-        previous_gameweek_squads = FantasySquad.objects.filter(gameweek=previous_gameweek)
-        current_gameweek_squads = []
-        for squad in previous_gameweek_squads:
-            squad.pk = None
-            squad.gameweek = current_gameweek
-            if not self.is_exist_squad(squad):
-                c += 1
-                current_gameweek_squads.append(squad)
+        # c = 0
+        # current_gameweek = int(os.getenv('GAMEWEEK'))
+        # previous_gameweek = current_gameweek - 1
+        # previous_gameweek_squads = FantasySquad.objects.filter(gameweek=previous_gameweek)
+        # current_gameweek_squads = []
+        # for squad in previous_gameweek_squads:
+        #     squad.pk = None
+        #     squad.gameweek = current_gameweek
+        #     if not self.is_exist_squad(squad):
+        #         c += 1
+        #         current_gameweek_squads.append(squad)
         
-        FantasySquad.objects.bulk_create(current_gameweek_squads)
-        print(f'{c} squads created in gameweek {current_gameweek}')
+        # FantasySquad.objects.bulk_create(current_gameweek_squads)
+        # print(f'{c} squads created in gameweek {current_gameweek}')
+        Player.objects.create(playerName="Test", playingRole="Captain", teamName="test")
         
