fab78b3dc1ee1084e8b794166447a3566653dd92
Yasser Mohsen <yassermohsen101010@gmail.com>
2022-06-05 13:34:54 +0200

    enhance alarms and notifications

--- generic/models.py
+++ generic/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.forms import DateTimeField
-from django.db.models import Model, CharField, DateTimeField, TextField
+from django.db.models import Model, CharField, DateTimeField, TextField, URLField
 
 #Table for Alarm
 class Alarm(Model):
-    alarm_header = CharField(max_length=100, unique=True)
-    alarm_content = TextField()
-    alarm_date = DateTimeField()
+    header = CharField(max_length=100, unique=True)
+    content = TextField()
+    date = DateTimeField()
+    url = URLField(max_length=1000, blank=True, null=True)
+    icon_tag = CharField(max_length=50, default="fa-bell", blank=True, null=True)
 
 #Table for Message
 class Notifications(Model):
-    notifications_header = CharField(max_length=100, unique=True)
-    notifications_content = TextField()
-    notifications_date = DateTimeField()
+    header = CharField(max_length=100, unique=True)
+    content = TextField()
+    date = DateTimeField()
+    url = URLField(max_length=1000, blank=True, null=True)
+    icon_tag = CharField(max_length=50, default="fa-envelope", blank=True, null=True)
